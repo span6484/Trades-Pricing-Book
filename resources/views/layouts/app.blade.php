fb13b8f6bccd5e0b97bac622189c2a2232fcf764
davik1000 <19306878@student.westernsydney.edu.au>
2020-09-08 21:22:11 +1000

    edit modal to customer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,9 +57,7 @@
           </button>
         </div>
       </div>
-      
 
-      <!-- <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas> -->
       @yield('content')
     </main>
   </div>
