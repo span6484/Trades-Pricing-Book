55151b313af1c8d3a224d3f1d5aa7293fd6716fc
davik1000 <19306878@student.westernsydney.edu.au>
2020-09-09 11:46:47 +1000

    edit customers

--- resources/views/customers.blade.php
+++ resources/views/customers.blade.php
@@ -10,12 +10,12 @@
 </button>
 
 <!-- Modal -->
-<div class="modal fade" id="customerModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+<div class="modal fade" id="customerModal" tabindex="-1" role="dialog" aria-labelledby="customerModalLabel"
     aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Enter customer details</h5>
+                <h5 class="modal-title" id="customerModalLabel">Enter customer details</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -24,54 +24,52 @@
                 <form>
                     <div class="form-row">
                         <div class="form-group col-md-6">
-                            <label for="input">Name</label>
-                            <input type="text" class="form-control" id="inputEmail4" placeholder="Email">
+                            <!-- <label for="input">Name</label> -->
+                            <input type="text" class="form-control" id="inputName" placeholder="Customer name">
                         </div>
                         <div class="form-group col-md-6">
-                            <label for="inputPassword4">Password</label>
-                            <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+                        <!-- <label for="inputEmail4">Email</label> -->
+      <input type="email" class="form-control" id="inputEmail" placeholder="Email">
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="inputAddress">Address</label>
-                        <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+                        <!-- <label for="inputAddress">Phone</label> -->
+                        <input type="text" class="form-control" id="inputPhone" placeholder="Phone">
                     </div>
                     <div class="form-group">
-                        <label for="inputAddress2">Address 2</label>
-                        <input type="text" class="form-control" id="inputAddress2"
-                            placeholder="Apartment, studio, or floor">
+                        <!-- <label for="inputAddress2">Address </label> -->
+                        <input type="text" class="form-control" id="inputAddress"
+                        placeholder="Address">
                     </div>
-                    <div class="form-row">
-                        <div class="form-group col-md-6">
+                    <!-- <div class="form-row"> -->
+                        <!-- <div class="form-group col-md-6">
                             <label for="inputCity">City</label>
                             <input type="text" class="form-control" id="inputCity">
-                        </div>
-                        <div class="form-group col-md-4">
-                            <label for="inputState">State</label>
-                            <select id="inputState" class="form-control">
-                                <option selected>Choose...</option>
-                                <option>...</option>
+                        </div> -->
+                        <div class="form-group">
+                            <select id="inputDiscount" class="form-control">
+                                <option selected>NORMAL PRICING - NO DISCOUNT</option>
+                                <option>MATES RATES CATEGORY 1</option>
                             </select>
                         </div>
-                        <div class="form-group col-md-2">
+                        <!-- <div class="form-group col-md-2">
                             <label for="inputZip">Zip</label>
                             <input type="text" class="form-control" id="inputZip">
-                        </div>
-                    </div>
-                    <div class="form-group">
+                        </div> -->
+                    <!-- </div> -->
+                    <!-- <div class="form-group">
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" id="gridCheck">
                             <label class="form-check-label" for="gridCheck">
                                 Check me out
                             </label>
                         </div>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Sign in</button>
+                    </div> -->
                 </form>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-primary">Save Customer</button>
             </div>
         </div>
     </div>
