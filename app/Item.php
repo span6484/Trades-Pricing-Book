a97da74df61de8a957512cdf860942c92c9067b6
davik1000 <19306878@student.westernsydney.edu.au>
2020-09-17 18:29:04 +1000

    misc

--- app/Item.php
+++ app/Item.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-    //
+    protected $table = 'items';
+    protected $primaryKey = 'pk_item_id';
+    protected $fillable = [
+        'material_itemcode',
+        'material_description',
+        'material_cost',
+        'fk_supplier_id'
+    ];
+
+    protected $attributes = [
+        'material_archived' => 0
+        ];
+
 }
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -26,13 +26,10 @@
                     Management</a>
             </li>
             <li>
-                <a href="#" class="dropdown-item list-group-item list-group-item-action bg-light border-0 pl-4">FORELSE LOOP
+                <a href="#" class="dropdown-item list-group-item list-group-item-action bg-light border-0 pl-4">FOR LOOP
                     CATEGORIES</a>
             </li>
-            <li>
-                <a href="#" class="dropdown-item list-group-item list-group-item-action bg-light border-0 pl-4">FORELSE LOOP
-                    NO CATEGORIES</a>
-            </li>
+            <!-- CATEGORY FOR LOOP GOES HERE -->
         </ul>
         <a href="#costsSubmenu" data-toggle="collapse" aria-expanded="false"
             class="list-group-item list-group-item-action bg-light dropdown-toggle dropdown-menu border-bottom-0">Costs
