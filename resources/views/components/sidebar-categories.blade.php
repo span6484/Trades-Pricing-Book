f009885a6c75108bffa22d331d8dfa716e52ea69
davik1000 <19306878@student.westernsydney.edu.au>
2020-09-23 16:40:38 +1000

    push test

--- resources/views/components/sidebar-categories.blade.php
+++ resources/views/components/sidebar-categories.blade.php
@@ -4,6 +4,5 @@
 
     @foreach($categories $category)
 
-    {{!! $category->category_name}}
         <li><a href="#" class="dropdown-item list-group-item list-group-item-action bg-light border-0 pl-4" value="{{ $category->pk_category_id }}">{{ $category->category_name }}</</a></li>
     @endforeach
