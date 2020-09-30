290e53ba77002a02b40c5e8bbc112ae1f0438d75
shaoqiu Pan <span6484@uni.sydney.edu.au>
2020-09-30 21:06:17 +1000

    new change

--- resources/views/grossmargin.blade.php
+++ resources/views/grossmargin.blade.php
@@ -120,11 +120,11 @@
                 @endforeach
 
                 @php
-                $NonChargeableOfficeStaffWages = 33244.20;
+                $NonChargeableOfficeStaffWages = 0;
                 @endphp
 
                 @php
-                $highestPaid = 65000;
+                $highestPaid = 0;
                 @endphp
 
                 @php
